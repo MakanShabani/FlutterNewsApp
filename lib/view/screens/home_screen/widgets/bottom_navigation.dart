0407864f492bd529769a326e292649764d097adb
Makan Shabani <makandeveloping@gmail.com>
2022-07-16 22:57:38 +0430

    Add bottomNavigation Config in HomeScreen

---
+++ lib/view/screens/home_screen/widgets/bottom_navigation.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+List<BottomNavigationBarItem> bottomNavigationBarItems =
+    const <BottomNavigationBarItem>[
+  BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+  BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+  BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Bookmark'),
+  BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settingss'),
+];
