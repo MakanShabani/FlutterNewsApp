3f91e55ee7d7e027f849898075cd22d5baff29dd
Makan Shabani <makandeveloping@gmail.com>
2022-07-16 22:53:24 +0430

    Add Appbar Theme Add BottomNavigation Theme Update App Title

--- lib/app.dart
+++ lib/app.dart
@@ -19,9 +19,32 @@ class App extends StatelessWidget {
             authenticationRepository:
                 context.read<FakeAuthenticationRepository>()),
         child: MaterialApp(
-          title: 'Admin Dashboard',
+          title: 'News App',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            // Define the default brightness and colors.
+            brightness: Brightness.light,
+            primarySwatch: Colors.orange,
+            primaryColor: Colors.orange,
+            appBarTheme: const AppBarTheme().copyWith(
+                backgroundColor: Colors.white,
+                iconTheme: const IconThemeData().copyWith(
+                  color: Colors.orange,
+                )),
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData()
+                .copyWith(
+                    type: BottomNavigationBarType.fixed,
+                    selectedItemColor: Colors.orange,
+                    unselectedItemColor: Colors.black54),
+            // Define the default font family.
+            fontFamily: 'Georgia',
+
+            // Define the default `TextTheme`. Use this to specify the default
+            // text styling for headlines, titles, bodies of text, and more.
+            textTheme: const TextTheme(
+              headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+              headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+              bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+            ),
           ),
           onGenerateRoute: AppRouter.generateRoute,
         ),
