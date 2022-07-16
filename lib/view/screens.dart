da945cd840b4a4481f179819ea726d31604d5dae
Makan Shabani <makandeveloping@gmail.com>
2022-07-16 22:52:27 +0430

    Update Screen Names

--- lib/view/screens.dart
+++ lib/view/screens.dart
@@ -1,2 +1,2 @@
-export 'screens/home_screen/home_page.dart';
-export 'screens/login_screen/login_page.dart';
+export 'screens/home_screen/home_screen.dart';
+export 'screens/login_screen/login_screen.dart';
