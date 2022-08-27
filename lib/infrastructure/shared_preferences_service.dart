1dde12206af84e74bcb0dd76ba8f4df74ea4b39c
Makan Shabani <makandeveloping@gmail.com>
2022-08-27 12:22:19 +0430

    Change SharedPrefrencesService from static to normal class

--- lib/infrastructure/shared_preferences_service.dart
+++ lib/infrastructure/shared_preferences_service.dart
@@ -5,23 +5,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/entities/entities.dart';
 
 class SharedPreferencesService {
-  static const String userInfoKey = 'user';
+  final String userInfoKey = 'user';
 
-  static Future<bool> saveUserInfo(AuthenticatedUserModel user) async {
+  Future<bool> saveUserInfo(AuthenticatedUserModel user) async {
     // Obtain shared preferences.
     final prefs = await SharedPreferences.getInstance();
 
     return await prefs.setString(userInfoKey, jsonEncode(user.toJson()));
   }
 
-  static Future<bool> removeUserInfo() async {
+  Future<bool> removeUserInfo() async {
     // Obtain shared preferences.
     final prefs = await SharedPreferences.getInstance();
 
     return await prefs.remove(userInfoKey);
   }
 
-  static Future<AuthenticatedUserModel?> getUserInfo() async {
+  Future<AuthenticatedUserModel?> getUserInfo() async {
     // Obtain shared preferences.
     final prefs = await SharedPreferences.getInstance();
 
