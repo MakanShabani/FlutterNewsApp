b578206bc02129bff03d1698760e14ecd4e7132f
Makan Shabani <makandeveloping@gmail.com>
2022-10-06 22:18:51 +0330

    Rename ViewConstants fields

--- lib/view/view_constants.dart
+++ lib/view/view_constants.dart
@@ -1,2 +1,2 @@
-const double screensContentsHorizontalPadding = 8.0;
+const double horizontalPadding = 8.0;
 const double circularBorderRadious = 10.0;
--- lib/view/widgets/posts_list_section.dart
+++ lib/view/widgets/posts_list_section.dart
@@ -24,9 +24,9 @@ class PostsListSection extends StatelessWidget {
           childCount: items.length,
           (context, index) => PostItemInVerticalList(
                 topPadding: 15,
-                rightPadding: view_constants.screensContentsHorizontalPadding,
+                rightPadding: view_constants.horizontalPadding,
                 bottoMargin: 10.0,
-                leftPadding: view_constants.screensContentsHorizontalPadding,
+                leftPadding: view_constants.horizontalPadding,
                 borderRadious: view_constants.circularBorderRadious,
                 item: items[index],
                 onPostBookMarkUpdated: (postId, newBookmarkValue) =>
