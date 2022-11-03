040862e9b4265b3e313d9d62e32f1bfceadae795
Makan Shabani <makandeveloping@gmail.com>
2022-11-03 22:13:04 +0330

    Add text constants to orgnize title and text globally

---
+++ lib/text_constants.dart
@@ -0,0 +1,3 @@
+const notSignedInTitle = 'You Are Not Signed In';
+const notSignedInSubtitle =
+    "To use this section and all of the other app's features you need to sign in to your account first.";
