9745bbba08e06cf01441fb859dc7b684d049dc04
Makan Shabani <makandeveloping@gmail.com>
2022-11-13 21:44:24 +0330

    Orgnize bookmark section widgets and blocs

--- lib/view/screens/home_screen/widgets/bookmark_section/bookmark_section.dart
+++ lib/view/screens/home_screen/widgets/bookmark_section/bookmark_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../bloc/blocs.dart';
+import '../../../../../repositories/repo_fake_implementaion/fake_repositories.dart';
 import '../../../../../router/route_names.dart';
 import '../../../../view_constants.dart';
 import '../../../../widgets/widgest.dart';
@@ -22,9 +23,52 @@ class BookmarkSection extends StatelessWidget {
             builder: (context, state) {
           if (state is LoggedIn) {
             //Show user's bookmarks
+            return BlocProvider(
+              create: (context) => BookmarkedPostsListCubit(
+                  haowManyPostToFetchEachTime: 10,
+                  postRepository: context.read<FakePostReposiory>())
+                ..fetch(userToken: state.user.token),
+              child: BlocBuilder<BookmarkedPostsListCubit,
+                  BookmarkedPostsListState>(
+                builder: (context, state) {
+                  if (state is BookmarkedPostsListFetchingSuccessful) {
+                    return PostsListSection(items: state.posts);
+                  } else if (state is BookmarkedPostsListFetchingHasError) {
+                    //we show error (bnner/snackbar) with list of previous fetched-posts
+                    if (state.previousLoadedPosts != null) {
+                      return PostsListSection(
+                          items: state.previousLoadedPosts!);
+                    }
+                    //first fetching has ended with error. there is no posts has been fetched yet to display
+                    //so we show error widget(in the center of page or as dialog)
+                    return const SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(
+                          child: Text('First fetching has ended with error')),
+                    );
+                  } else if (state is BookmarkedPostsListFetching) {
+                    if (state.previousLoadedPosts != null) {
+                      //we show loading indicator at the end of the previously fetched-posts
+                      return PostsListSection(
+                          items: state.previousLoadedPosts!);
+                    }
+                    //this first fetching. there is no posts has been fetched yet to display
+                    //so we show loading indicator widget(in the center of page or as dialog)
+                    return const SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(child: Text('First fetching Loading')),
+                    );
+                  }
 
-            return SliverFixedExtentList(
-                delegate: SliverChildListDelegate([]), itemExtent: 160);
+                  //Initial State
+                  return const SliverFillRemaining(
+                    hasScrollBody: false,
+                    child:
+                        Center(child: Text('Initia State -- First Fetching')),
+                  );
+                },
+              ),
+            );
           } else {
             //Show warning widget that user must be signed in to use bookmark section
 
