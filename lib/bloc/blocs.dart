c09aa2dc593f91366bfd9103df057b5ba6abdcf0
Makan Shabani <makandeveloping@gmail.com>
2022-11-22 21:30:30 +0330

    Change TabContentBLoc to PostsListCubit
    Now this cubit is the only cubit for fetch posts

--- lib/bloc/blocs.dart
+++ lib/bloc/blocs.dart
@@ -3,5 +3,5 @@ export './home_section/home_section_bloc.dart';
 export './image_cubit/./image_cubit.dart';
 export './post_bookmark_cubit/post_bookmark_cubit.dart';
 export './theme_cubit/theme_cubit.dart';
-export './post_list_bloc/posts_list_bloc.dart';
+export 'post_list_cubit/posts_list_cubit.dart';
 export './bookmarked_posts_list_cubit/bookmarked_posts_list_cubit.dart';
