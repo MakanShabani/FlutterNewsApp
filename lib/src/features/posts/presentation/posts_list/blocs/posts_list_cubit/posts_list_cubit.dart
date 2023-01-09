5cf561e7db34b514bdbe353adba99dd9e2556577
Makan Shabani <makandeveloping@gmail.com>
2023-01-09 21:00:28 +0330

    Update PostsListCubit and PostsListCubitState
    -Now PostsListCubitState have posts and categoryId fields, which inhereted states use them

--- lib/src/features/posts/presentation/posts_list/blocs/posts_list_cubit/posts_list_cubit.dart
+++ lib/src/features/posts/presentation/posts_list/blocs/posts_list_cubit/posts_list_cubit.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-import 'package:collection/collection.dart';
 
 import '../../../../../../infrastructure/shared_dtos/shared_dtos.dart';
 import '../../../../../../infrastructure/shared_models/shared_model.dart';
 import '../../../../application/posts_services.dart';
 import '../../../../domain/posts_models.dart';
-
+import 'package:collection/collection.dart';
 part 'posts_list_cubit_state.dart';
 
 class PostsListCubit extends Cubit<PostsListCubitState> {
@@ -16,7 +15,7 @@ class PostsListCubit extends Cubit<PostsListCubitState> {
     required this.haowManyPostFetchEachTime,
   })  : _postsListService = postsListService,
         assert(haowManyPostFetchEachTime >= 10),
-        super(PostsListCubitInitial());
+        super(PostsListCubitInitial(posts: List.empty()));
 
   final PostsListService _postsListService;
   final int haowManyPostFetchEachTime;
@@ -27,14 +26,11 @@ class PostsListCubit extends Cubit<PostsListCubitState> {
       return;
     }
 
-    //define pagingOptionsVm to use in fetching
-    List<Post> currentPosts = allPosts();
-    PagingOptionsDTO toFetchPagingOptions = PagingOptionsDTO(
-        offset: currentPosts.length, limit: haowManyPostFetchEachTime);
     emit(PostsListCubitFetching(
-      toLoadPagingOptionsVm: toFetchPagingOptions,
+      toLoadPagingOptionsVm: PagingOptionsDTO(
+          offset: state.posts.length, limit: haowManyPostFetchEachTime),
       categoryId: categoryId,
-      currentPosts: currentPosts,
+      posts: state.posts,
     ));
 
     //load posts
@@ -46,116 +42,49 @@ class PostsListCubit extends Cubit<PostsListCubitState> {
         userToken != null) {
       //fetch user bookmarked posts
       fetchingPostsResponse = await _postsListService.getUserBookmarkedPosts(
-          userToken: userToken, pagingOptionsDTO: toFetchPagingOptions);
+          userToken: userToken,
+          pagingOptionsDTO:
+              (state as PostsListCubitFetching).toLoadPagingOptionsVm);
     } else {
       //fetch all posts(including user bookmarked posts)
       fetchingPostsResponse = await _postsListService.getPosts(
-          pagingOptionsDTO: toFetchPagingOptions,
+          pagingOptionsDTO:
+              (state as PostsListCubitFetching).toLoadPagingOptionsVm,
           userToken: userToken,
           categoryId: categoryId);
     }
 
     if (fetchingPostsResponse.statusCode != 200) {
       emit(PostsListCubitFetchingHasError(
-          toLoadPagingOptionsVm: toFetchPagingOptions,
-          currentPosts: currentPosts,
+          toLoadPagingOptionsVm:
+              (state as PostsListCubitFetching).toLoadPagingOptionsVm,
+          posts: state.posts,
           categoryId: categoryId,
           error: fetchingPostsResponse.error!));
       return;
     }
 
     emit(PostsListCubitFetchedSuccessfully(
-      previousPosts: currentPosts,
-      newDownloadedPosts: fetchingPostsResponse.data!,
+      posts: state.posts + fetchingPostsResponse.data!,
+      lastLoadedPagingOptionsDto:
+          (state as PostsListCubitFetching).toLoadPagingOptionsVm,
       categoryId: categoryId,
     ));
   }
 
   void removePostFromList({required String postId}) {
-    List<Post> currentPosts = allPosts();
     Post removedPost =
-        currentPosts.removeAt(currentPosts.indexWhere((p) => p.id == postId));
+        state.posts.removeAt(state.posts.indexWhere((p) => p.id == postId));
     emit(PostsListCubitPostHasBeenRemoved(
-        posts: currentPosts,
+        posts: state.posts,
         removedPost: removedPost,
-        categoryId: getCategoryId()));
+        categoryId: state.categoryId));
   }
 
   void updatePostBookmarkStatusWithoutChangingState(
       String postId, bool newBookmarkStatus) {
-    if (state is PostsListCubitFetching) {
-      (state as PostsListCubitFetching)
-          .currentPosts
-          .firstWhereOrNull((p) => p.id == postId)
-          ?.isBookmarked = newBookmarkStatus;
-      return;
-    }
-    if (state is PostsListCubitFetchedSuccessfully) {
-      int postIndex = (state as PostsListCubitFetchedSuccessfully)
-          .previousPosts
-          .indexWhere((p) => p.id == postId);
-      if (postIndex != -1) {
-        (state as PostsListCubitFetchedSuccessfully)
-            .previousPosts[postIndex]
-            .isBookmarked = newBookmarkStatus;
-        return;
-      }
-      postIndex = (state as PostsListCubitFetchedSuccessfully)
-          .newDownloadedPosts
-          .indexWhere((p) => p.id == postId);
-      if (postIndex != -1) {
-        (state as PostsListCubitFetchedSuccessfully)
-            .newDownloadedPosts[postIndex]
-            .isBookmarked = newBookmarkStatus;
-        return;
-      }
-
-      return;
-    }
-    if (state is PostsListCubitFetchingHasError) {
-      (state as PostsListCubitFetchingHasError)
-          .currentPosts
-          .firstWhereOrNull((p) => p.id == postId)
-          ?.isBookmarked = newBookmarkStatus;
-    }
-  }
-
-  List<Post> allPosts() {
-    if (state is PostsListCubitFetching) {
-      return (state as PostsListCubitFetching).currentPosts;
-    }
-    if (state is PostsListCubitFetchedSuccessfully) {
-      return (state as PostsListCubitFetchedSuccessfully).previousPosts +
-          (state as PostsListCubitFetchedSuccessfully).newDownloadedPosts;
-    }
-    if (state is PostsListCubitFetchingHasError) {
-      return (state as PostsListCubitFetchingHasError).currentPosts;
-    }
-    if (state is PostsListCubitPostHasBeenAdded) {
-      return (state as PostsListCubitPostHasBeenAdded).posts;
-    }
-    if (state is PostsListCubitPostHasBeenRemoved) {
-      return (state as PostsListCubitPostHasBeenRemoved).posts;
-    }
-
-    return List.empty();
-  }
-
-  String? getCategoryId() {
-    if (state is PostsListCubitFetching) {
-      return (state as PostsListCubitFetching).categoryId;
-    }
-    if (state is PostsListCubitFetchedSuccessfully) {
-      return (state as PostsListCubitFetchedSuccessfully).categoryId;
-    }
-    if (state is PostsListCubitFetchingHasError) {
-      return (state as PostsListCubitFetchingHasError).categoryId;
-    }
-    if (state is PostsListCubitPostHasBeenAdded) {
-      return (state as PostsListCubitPostHasBeenAdded).categoryId;
-    }
-    if (state is PostsListCubitPostHasBeenRemoved) {
-      return (state as PostsListCubitPostHasBeenRemoved).categoryId;
-    }
+    state.posts.firstWhereOrNull((p) => p.id == postId)?.isBookmarked =
+        newBookmarkStatus;
+    return;
   }
 }
--- lib/src/features/posts/presentation/posts_list/blocs/posts_list_cubit/posts_list_cubit_state.dart
+++ lib/src/features/posts/presentation/posts_list/blocs/posts_list_cubit/posts_list_cubit_state.dart
@@ -1,68 +1,64 @@
 part of 'posts_list_cubit.dart';
 
 @immutable
-abstract class PostsListCubitState {}
+abstract class PostsListCubitState {
+  const PostsListCubitState({this.categoryId, required this.posts});
+  final String? categoryId;
+  final List<Post> posts;
+}
 
-class PostsListCubitInitial extends PostsListCubitState {}
+class PostsListCubitInitial extends PostsListCubitState {
+  const PostsListCubitInitial({required super.posts, super.categoryId});
+}
 
 class PostsListCubitFetching extends PostsListCubitState {
-  final List<Post> currentPosts;
   final PagingOptionsDTO toLoadPagingOptionsVm;
-  final String? categoryId;
 
-  PostsListCubitFetching({
-    required this.currentPosts,
+  const PostsListCubitFetching({
+    required super.posts,
     required this.toLoadPagingOptionsVm,
-    this.categoryId,
+    super.categoryId,
   });
 }
 
 class PostsListCubitFetchedSuccessfully extends PostsListCubitState {
-  final List<Post> previousPosts;
-  final List<Post> newDownloadedPosts;
-  final String? categoryId;
+  final PagingOptionsDTO lastLoadedPagingOptionsDto;
 
-  PostsListCubitFetchedSuccessfully({
-    required this.previousPosts,
-    required this.newDownloadedPosts,
-    this.categoryId,
+  const PostsListCubitFetchedSuccessfully({
+    required super.posts,
+    required this.lastLoadedPagingOptionsDto,
+    super.categoryId,
   });
 }
 
 class PostsListCubitFetchingHasError extends PostsListCubitState {
-  final List<Post> currentPosts;
   final PagingOptionsDTO toLoadPagingOptionsVm;
-  final String? categoryId;
   final ErrorModel error;
 
-  PostsListCubitFetchingHasError({
-    required this.currentPosts,
+  const PostsListCubitFetchingHasError({
+    required super.posts,
     required this.toLoadPagingOptionsVm,
-    this.categoryId,
+    super.categoryId,
     required this.error,
   });
 }
 
 class PostsListCubitPostHasBeenRemoved extends PostsListCubitState {
-  final List<Post> posts;
   final Post removedPost;
-  final String? categoryId;
 
-  PostsListCubitPostHasBeenRemoved({
-    required this.posts,
+  const PostsListCubitPostHasBeenRemoved({
+    required super.posts,
     required this.removedPost,
-    this.categoryId,
+    super.categoryId,
   });
 }
 
 class PostsListCubitPostHasBeenAdded extends PostsListCubitState {
-  final List<Post> posts;
   final Post addedPost;
-  final String? categoryId;
 
-  PostsListCubitPostHasBeenAdded({
-    required this.posts,
+  const PostsListCubitPostHasBeenAdded({
+    required super.posts,
     required this.addedPost,
-    this.categoryId,
+    super.categoryId,
   });
 }
