6cd2c74626991becc06345e328b0cd11dacc4f19
Makan Shabani <makandeveloping@gmail.com>
2023-01-25 12:26:03 +0330

    Create BookmarkSection
    -Add a scroll listener to the page
    -Add bloc providers to the page

--- lib/src/features/posts/presentation/posts_list/bookmark_section.dart
+++ lib/src/features/posts/presentation/posts_list/bookmark_section.dart
@@ -1,4 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/src/common_widgets/common_widgest.dart';
+import 'package:responsive_admin_dashboard/src/features/authentication/presentation/authentication_presentations.dart';
+import 'package:responsive_admin_dashboard/src/infrastructure/constants.dart/constants.dart';
+import 'package:responsive_admin_dashboard/src/router/route_names.dart';
+import '../../application/posts_list_service.dart';
+import '../../data/repositories/fake_posts_list_repository.dart';
+import 'blocs/posts_list_blocs.dart';
 
 class BookmarkSection extends StatefulWidget {
   const BookmarkSection({Key? key}) : super(key: key);
@@ -10,12 +18,38 @@ class BookmarkSection extends StatefulWidget {
 class _BookmarkSectionState extends State<BookmarkSection>
     with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
   late ScrollController _scrollController;
+  late PostsListCubit _postsListCubit;
+  late ListNotifireCubit _postslistNotifireCubit;
 
   @override
   void initState() {
     super.initState();
     _scrollController = ScrollController();
-    //_scrollController.addListener(scrollListenrer);
+    _scrollController.addListener(scrollListenrer);
+
+    _postsListCubit = PostsListCubit(
+        postsListService:
+            PostsListService(postRepository: context.read<FakePostReposiory>()),
+        haowManyPostFetchEachTime: 10);
+    if (context.read<AuthenticationCubit>().state is AuthenticationLoggedIn) {
+      _postsListCubit.fetch(
+          (context.read<AuthenticationCubit>().state as AuthenticationLoggedIn)
+              .user
+              .token,
+          null,
+          true);
+    }
+
+    _postslistNotifireCubit = ListNotifireCubit();
+  }
+
+  @override
+  void dispose() {
+    _postslistNotifireCubit.close();
+    _postsListCubit.close();
+    _scrollController.removeListener(scrollListenrer);
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -26,6 +60,45 @@ class _BookmarkSectionState extends State<BookmarkSection>
     //Notice the super-call here.
     super.build(context);
 
-    return Container();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: _postsListCubit),
+        BlocProvider.value(value: _postslistNotifireCubit),
+      ],
+      child: Container(),
+    );
+  }
+
+  void scrollListenrer() {
+    if (_postsListCubit.state is PostsListCubitFetchedSuccessfully ||
+        (_postsListCubit.state is PostsListCubitFetchingHasError &&
+            (_postsListCubit.state as PostsListCubitFetchingHasError)
+                    .failedLoadPagingOptionsVm
+                    .offset >
+                0)) {
+      if (_scrollController.offset ==
+          _scrollController.position.maxScrollExtent) {
+        //fetch new posts
+        fetchPosts();
+      }
+    }
+  }
+
+  void fetchPosts() {
+    if (context.read<AuthenticationCubit>().state is! AuthenticationLoggedIn) {
+      ScaffoldMessenger.of(context).showSnackBar(appSnackBar(
+        context: context,
+        message: error401SnackBar,
+        actionLabel: 'Sign In',
+        action: () => Navigator.pushNamed(context, loginRoute),
+      ));
+      return;
+    }
+    _postsListCubit.fetch(
+        (context.read<AuthenticationCubit>().state as AuthenticationLoggedIn)
+            .user
+            .token,
+        null,
+        true);
   }
 }
