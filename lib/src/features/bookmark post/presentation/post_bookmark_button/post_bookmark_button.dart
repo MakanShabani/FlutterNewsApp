6dff4b73643f861730a70e630c87b4c9b0a4ce20
Makan Shabani <makandeveloping@gmail.com>
2023-02-06 14:31:02 +0330

    Refactor PostBookmarkButton

--- lib/src/features/bookmark post/presentation/post_bookmark_button/post_bookmark_button.dart
+++ lib/src/features/bookmark post/presentation/post_bookmark_button/post_bookmark_button.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:responsive_admin_dashboard/src/infrastructure/constants.dart/constants.dart';
-import 'package:responsive_admin_dashboard/src/router/route_names.dart';
 
 import '../../../../common_widgets/common_widgest.dart';
 import '../../../../infrastructure/utils/utils.dart';
-import '../../../authentication/presentation/blocs/authentication_cubit.dart';
 import '../../../posts/domain/posts_models.dart';
 import 'post_bookmark_cubit/post_bookmark_cubit.dart';
 
@@ -18,6 +15,7 @@ class PostBookmarkButton extends StatefulWidget {
     this.unBookmarkedColor,
     this.loadingSize,
     this.onPostBookmarkUpdated,
+    this.onBookmarkPressed,
   }) : super(key: key);
 
   final Post post;
@@ -27,6 +25,7 @@ class PostBookmarkButton extends StatefulWidget {
   final double? loadingSize;
 
   final CustomeValueSetterCallback<Post, bool>? onPostBookmarkUpdated;
+  final CustomeValueSetterCallback<Post, bool>? onBookmarkPressed;
   @override
   State<PostBookmarkButton> createState() => _PostBookmarkButtonState();
 }
@@ -44,83 +43,43 @@ class _PostBookmarkButtonState extends State<PostBookmarkButton> {
   Widget build(BuildContext context) {
     return BlocConsumer<PostBookmarkCubit, PostBookmarkState>(
       listenWhen: (previous, current) =>
-          whenListenToPostBookCubitStates(widget.post.id, current),
-      listener: (context, state) => onPostBookmarkUpdated(widget.post,
+          current is PostBookmarkUpdatedSuccessfullyState &&
+          current.post.id == widget.post.id,
+      listener: (context, state) => onPostBookmarkUpdated(
           (state as PostBookmarkUpdatedSuccessfullyState).newBookmarkValue),
-      buildWhen: (previous, current) =>
-          whenPostItemShouldWidgetRebuild(widget.post.id, current),
+      buildWhen: (previous, current) => whenRebuild(current),
       builder: (context, state) {
         return BookmarkButton(
           unBookmarkedColor: widget.unBookmarkedColor,
           bookmarkedColor: widget.bookmarkedColor,
-          isLoading: isItemBookmarking(widget.post.id, state),
+          isLoading: isItemBookmarking(state),
           isBoolmarked: isBookmarked,
-          onPressed: onBookmarkButtonPressed,
+          onPressed: () => widget.onBookmarkPressed != null
+              ? widget.onBookmarkPressed!(widget.post, !isBookmarked)
+              : null,
         );
       },
     );
   }
 
-  bool whenListenToPostBookCubitStates(String postId, PostBookmarkState state) {
-    return state is PostBookmarkUpdatedSuccessfullyState &&
-        state.post.id == postId;
-  }
-
-  bool whenPostItemShouldWidgetRebuild(String postId, PostBookmarkState state) {
+  bool whenRebuild(PostBookmarkState state) {
     return (state is PostBookmarkUpdatingPostBookmarkState &&
-            state.post.id == postId) ||
+            state.post.id == widget.post.id) ||
         (state is PostBookmarkUpdatedSuccessfullyState &&
-            state.post.id == postId) ||
-        (state is PostBookmarkUpdateHasErrorState && state.post.id == postId);
+            state.post.id == widget.post.id) ||
+        (state is PostBookmarkUpdateHasErrorState &&
+            state.post.id == widget.post.id);
   }
 
-  bool isItemBookmarking(String postId, PostBookmarkState state) {
-    return state is PostBookmarkUpdatingPostBookmarkState &&
-            state.post.id == postId
-        ? true
-        : state is PostBookmarkUpdatedSuccessfullyState &&
-                state.post.id == postId
-            ? false
-            : state is PostBookmarkUpdateHasErrorState &&
-                    state.post.id == postId
-                ? false
-                : state.currentBookmarkingPosts.contains(postId);
+  bool isItemBookmarking(PostBookmarkState state) {
+    return state.currentBookmarkingPosts.contains(widget.post.id);
   }
 
-  void onPostBookmarkUpdated(Post postId, bool newBookmarkStatus) {
+  void onPostBookmarkUpdated(bool newBookmarkStatus) {
     isBookmarked = newBookmarkStatus;
 
     widget.onPostBookmarkUpdated != null
-        ? widget.onPostBookmarkUpdated!(postId, newBookmarkStatus)
+        ? widget.onPostBookmarkUpdated!(widget.post, newBookmarkStatus)
         : null;
-  }
-
-  void onBookmarkButtonPressed() {
-    if (context.read<AuthenticationCubit>().state is! AuthenticationLoggedIn) {
-      //Show a snackbar or a dialog to notify user that they have to login first
-      //in order to use this feature.
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        appSnackBar(
-          context: context,
-          message: error401SnackBar,
-          action: () {
-            ScaffoldMessenger.of(context).clearSnackBars();
-            Navigator.pushNamed(context, loginRoute);
-          },
-          actionLabel: 'Sign in',
-        ),
-      );
-
-      return;
-    }
-    context.read<PostBookmarkCubit>().toggleBookmark(
-          userToken: (context.read<AuthenticationCubit>().state
-                  as AuthenticationLoggedIn)
-              .user
-              .token,
-          post: widget.post,
-          newBookmarkValue: !isBookmarked,
-        );
   }
 }
