523370335fa20290278f3d3142d51c19f6e5fd8d
Makan Shabani <makandeveloping@gmail.com>
2023-03-06 20:36:16 +0330

    Add PostDetailsCubit to PostScreen

--- lib/src/features/posts/presentation/post_detail/post_screen.dart
+++ lib/src/features/posts/presentation/post_detail/post_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_admin_dashboard/src/features/posts/application/post_service.dart';
+import 'package:responsive_admin_dashboard/src/features/posts/data/repositories/fake_posts_list_repository.dart';
+import 'package:responsive_admin_dashboard/src/features/posts/presentation/post_detail/cubit/post_details_cubit.dart';
 
 class PostScreen extends StatelessWidget {
   const PostScreen({Key? key, required this.postId}) : super(key: key);
@@ -7,6 +11,21 @@ class PostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PostDetailsCubit>(
+          create: (context) => PostDetailsCubit(
+            postService:
+                PostService(postRepository: context.read<FakePostReposiory>()),
+          )..fetchPostDetails(postId: postId),
+        )
+      ],
+      child: Scaffold(
+        body: SafeArea(
+            child: CustomScrollView(
+          slivers: [],
+        )),
+      ),
+    );
   }
 }
