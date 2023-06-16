af1f772fd2e5413093dee1ad79c33ea09a2b8e47
Makan Shabani <makandeveloping@gmail.com>
2023-06-16 16:57:12 +0330

    Create FakeCommentRepository
    -Implement getComments Function

---
+++ lib/src/features/comment/data/repositories/fake_comments_repository.dart
@@ -0,0 +1,33 @@
+import '../../../../infrastructure/shared_dtos/shared_dtos.dart';
+import '../../../../server_impementation/data_sources/comment_data_source.dart';
+import '../../domain/comment.dart';
+
+import '../dtos/send_comment_dto.dart';
+
+import 'comment_repository.dart';
+
+class FakeCommentsRepository implements CommentRepository {
+  FakeCommentsRepository(
+      {required this.delayDurationInSeconds,
+      required CommentDataSource commentDataSource})
+      : _commentDataSource = commentDataSource;
+  final CommentDataSource _commentDataSource;
+  final int delayDurationInSeconds;
+  @override
+  Future<ResponseDTO<List<Comment>>> getComments(
+      {required postId, required PagingOptionsDTO pagingOptionsDTO}) async {
+    return await Future.delayed(
+        Duration(seconds: delayDurationInSeconds),
+        () => _commentDataSource.getComments(
+            postId: postId, pagingOptionsDTO: pagingOptionsDTO));
+  }
+
+  @override
+  Future<ResponseDTO<void>> sendComment(
+      {required userToken,
+      required userId,
+      required SendCommentDTO sendCommentDTO}) {
+    // TODO: implement sendComment
+    throw UnimplementedError();
+  }
+}
