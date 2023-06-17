56b82e4a8a0e3eb056b08be29de12111097f534d
Makan Shabani <makandeveloping@gmail.com>
2023-06-17 21:23:20 +0330

    Add SendComment Service

---
+++ lib/src/features/comment/application/comment_application.dart
@@ -0,0 +1,1 @@
+export './send_comment_service.dart';
---
+++ lib/src/features/comment/application/send_comment_service.dart
@@ -0,0 +1,20 @@
+import 'package:responsive_admin_dashboard/src/features/comment/data/repositories/comment_repository.dart';
+
+import '../../../infrastructure/shared_dtos/shared_dtos.dart';
+import '../data/dtos/dtos.dart';
+
+class SendCommentService {
+  SendCommentService({required CommentRepository commentRepository})
+      : _commentRepository = commentRepository;
+  final CommentRepository _commentRepository;
+
+  Future<ResponseDTO<void>> sendComment(
+      {required String userToken,
+      required String userId,
+      required SendCommentDTO sendCommentDTO}) async {
+    //we write our core logic here to send a comment
+
+    return await _commentRepository.sendComment(
+        userToken: userToken, userId: userId, sendCommentDTO: sendCommentDTO);
+  }
+}
