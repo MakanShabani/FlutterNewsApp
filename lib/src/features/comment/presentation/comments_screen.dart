f6977cede674c5fd038cf3ef299adab17dcb3761
Makan Shabani <makandeveloping@gmail.com>
2023-06-23 22:50:52 +0330

    Add AppbarSection to CommentsScreen

--- lib/src/features/comment/presentation/comments_screen.dart
+++ lib/src/features/comment/presentation/comments_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/comment_widgets.dart';
+
 class CommentsScreen extends StatelessWidget {
   const CommentsScreen({Key? key, required this.postId}) : super(key: key);
   final String postId;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: CustomScrollView(
-        slivers: [],
+        //Appbar Section
+        slivers: [AppbarSection(title: 'Comments')],
       ),
     );
   }
---
+++ lib/src/features/comment/presentation/widgets/comment_widgets.dart
@@ -0,0 +1,1 @@
+export './appbar_section.dart';
